3795f5046041e551adcab3c0424dbaf44d1e2b42
SHIRIN <abedishirin@rocketmail.com>
2014-10-17 23:11:05 -0400

    change

--- testone/src/testone/Testing.java
+++ testone/src/testone/Testing.java
@@ -3,6 +3,9 @@ package testone;
 public class Testing {
 	public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        
     }
 
 }
